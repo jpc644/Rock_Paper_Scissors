0689ecf76b55f5d8b5b5ae6b9f0efe277ea18e33
jpc644 <jpc644@stern.nyu.edu>
2021-05-27 15:09:31 -0400

    Updated Display Results

--- game.py
+++ game.py
@@ -2,10 +2,10 @@
 
 import random
 
-print("Rock, Paper, Scissors, Shoot!")
+print("Welcome to Rock, Paper, Scissors, Shoot Player One!")
 
 user_choice = input("Please choose one of 'rock', 'paper', 'scissors':")
-print(user_choice)
+print("Player One selected:", user_choice)
 
 #  validate the input such that only if it is one of the expected values
 #....will we continue with the rest of the program
@@ -15,27 +15,27 @@ print(user_choice)
 #if user_choice = "rock" or "paper" or "scissors":
 
 if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors"):
-    print("valid. please continue")
+    print("Good choice! Please continue")
 else:
-    print("oops, invalid input.  please try again")
+    print("OOPS, invalid input.  Please try again")
     exit()
 
 valid_options = ["rock", "paper", "scissors"]
 computer_choice = random.choice(valid_options)
-print("computer_choice:", computer_choice)
+print("The Computer selected:", computer_choice)
 
 if (user_choice == "rock") and (computer_choice == "scissors"):
-    print("USER WINS")
+    print("Player One Wins, Congratulations!")
 elif (user_choice == "paper") and (computer_choice == "rock"):
-    print("USER WINS")
+    print("Player One Wins, Congratulations!")
 elif(user_choice == "scissors") and (computer_choice == "paper"):
-    print("USER WINS")
+    print("Player One Wins, Congratulations!")
 elif (user_choice == computer_choice):
-    print("TIE")
+    print("Tie!  Why not play again?!")
 else:
-    print("COMPUTER WINS")
+    print("The computer wins!  Sorry, better luck next time!")
 
 
 
 
-print("this is the end of our game.  please play again")
+print("This is the end of our game.  Please play again soon!")
