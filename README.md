011cc7e9061057195209bf95026b38d51471e09e
jpc644 <jpc644@stern.nyu.edu>
2021-05-30 08:35:31 -0400

    Added descriptions and read me file

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # Rock_Paper_Scissors
 Rock Paper Scissors Assignment for Python Coding Class (Class 2, Assignment 1)
+
+# Installation Process
+    Clone or download the Rock Paper Scissors repo onto your local device in a directory you can easily navigate to...
+    Navigate to the repo using your command line application so subsequent commands assume you are running them from the local repository's root directory:
+
+    '''sh
+    cd Rock_Paper_Scissors
+    '''
+
+    ## Setup
+
+    Setup a vritual environment
+
+    '''sh 
+    conda create -n my-rps-game-env python=3.8
+    conda activate my-rps-game-env
+    '''
+
+    Install the required packages:
+
+    '''sh
+    pip install -r requirements.txt
+    '''
+
+    ### Configuration of Environment Variables
+
+    Add a new ".env" file to the root directory of this repo, and place contents like the following inside:
+
+
+    '''
+    PLAYER_NAME = 'Input Your Player Name'
+
+    ## Usage
+
+    Run the Game:
+
+    '''sh
+    python game.py
+    '''
+
+# Payer must specify in .env file what their PLAYER_NAME is
+# Install the dotenv package
+# Load the appropriate dotenv variable
--- game.py
+++ game.py
@@ -11,13 +11,14 @@ dotenv.load_dotenv()
 PLAYER_NAME = os.getenv("Player_Name")
 print(PLAYER_NAME)
 
+print ("-------------------")
+#print ("Welcome" + PLAYER_NAME +"to Rock, Paper, Scissors, Shoot!")
 
 
-
-print("Welcome to Rock, Paper, Scissors, Shoot Player One!")
+print("Welcome to Rock, Paper, Scissors, Shoot,", PLAYER_NAME)
 
 user_choice = input("Please choose one of 'rock', 'paper', 'scissors':")
-print("Player One selected:", user_choice)
+print(PLAYER_NAME, "selected:", user_choice)
 
 #  validate the input such that only if it is one of the expected values
 #....will we continue with the rest of the program
@@ -27,9 +28,9 @@ print("Player One selected:", user_choice)
 #if user_choice = "rock" or "paper" or "scissors":
 
 if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors"):
-    print("Good choice! Please continue")
+    print("Good choice!")
 else:
-    print("OOPS, invalid input.  Please try again")
+    print("OOPS, invalid input.  Please try again", PLAYER_NAME)
     exit()
 
 valid_options = ["rock", "paper", "scissors"]
@@ -37,17 +38,17 @@ computer_choice = random.choice(valid_options)
 print("The Computer selected:", computer_choice)
 
 if (user_choice == "rock") and (computer_choice == "scissors"):
-    print("Player One Wins, Congratulations!")
+    print(PLAYER_NAME, "Wins, Congratulations!")
 elif (user_choice == "paper") and (computer_choice == "rock"):
-    print("Player One Wins, Congratulations!")
+    print(PLAYER_NAME, "Wins, Congratulations!")
 elif(user_choice == "scissors") and (computer_choice == "paper"):
-    print("Player One Wins, Congratulations!")
+    print(PLAYER_NAME, "Wins, Congratulations!")
 elif (user_choice == computer_choice):
-    print("Tie!  Why not play again?!")
+    print("Game is a Tie!  Why not play again?!")
 else:
-    print("The computer wins!  Sorry, better luck next time!")
+    print("The Computer wins!  Sorry, better luck next time", PLAYER_NAME)
 
 
 
 
-print("This is the end of our game.  Please play again soon!")
+print("This is the end of our game, thank you for playing!  Please play again soon!")
